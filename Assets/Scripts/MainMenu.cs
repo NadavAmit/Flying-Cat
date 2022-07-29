7fd3e00d6d12aaeb2d1dd46e4e8254f24aa04ae7
Amit Nadav <amit.nadav1@gmail.com>
2022-07-29 18:12:16 +0300

    Tutorial Created

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,6 +9,10 @@ public class MainMenu : MonoBehaviour
 
         SceneManager.LoadScene("Level1.0");
     }
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
 
     public void QuitGame() {
 
---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    private Button toStage2;
+    public void MainMenu() {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    void Start()
+    {
+        toStage2 = GetComponent<Button>();
+        toStage2 = GameObject.Find("FirstNextButton").GetComponent<Button>();
+        toStage2.onClick.AddListener(OnClickToStage2);
+    }
+
+    void OnClickToStage2()
+    {
+        Debug.Log("You have clicked the button!");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
